0df846b94de9b2ed1267cee7fef5b02573c7a644
ALI RAZA SHAHEB <89347148+AliRazaShaheb@users.noreply.github.com>
2022-04-11 22:19:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+const counter = document.getElementById("counter");
+const increment = document.getElementById("increment");
+const decrement = document.getElementById("decrement");
+const incrementOdd = document.getElementById("incrementOdd");
+const incrementAsync = document.getElementById("incrementAsync");
+
+let val = 0;
+
+increment.addEventListener("click", () => {
+  counter.innerHTML = val += 1;
+});
+
+decrement.addEventListener("click", () => {
+  counter.innerHTML = val === 0 ? 0 : (val -= 1);
+});
+
+incrementOdd.addEventListener("click", () => {
+  counter.innerHTML = val % 2 !== 0 ? (val += 1) : val;
+});
+incrementAsync.addEventListener("click", () => {
+  setTimeout(() => {
+    counter.innerHTML = val += 1;
+  }, 1000);
+});
